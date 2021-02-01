8afafae340778ff0c2cac84dd37ae517968404c3
femi-lawal <femilawal87@yahoo.com>
2021-02-01 07:39:03 +0100

    write question prompt for palindrome

---
+++ python/questions/palindrome.py
@@ -0,0 +1,9 @@
+# Write a function that takes in a non-empty string and that returns a boolean
+# representing whether the string is a palindrome.
+# A palindrome is defined as a string that's written the same forward and
+# backward. Note that single-character strings are palindromes.
+# it's written the same forward and backward
+# Start by building the input string in reverse order and comparing this newly built string to the input string. Can you do this without using string concatenations?
+# Can you optimize your algorithm by using recursion? What are the implications of recursion on an algorithm's space-time complexity analysis?
+# Go back to an iterative solution and try using pointers to solve this problem: start with a pointer at the first index of the string and a pointer at the final index of the string. What can you do from there?
+
