1e879f286320b93deeaf0fcbe5253ef577f60374
femi-lawal <femilawal87@yahoo.com>
2021-02-10 23:05:58 +0100

    added solution to selection sort

--- python/questions/selection_sort.py
+++ python/questions/selection_sort.py
@@ -1,17 +1,16 @@
-# <div class="_2cjRhfXbSATpeY6gWYgvJE"><p>
+# 
 #   Write a function that takes in an array of integers and returns a sorted
 #   version of that array. Use the Selection Sort algorithm to sort the array.
-# </p>
-# <p>
+# 
+# 
 #   If you're unfamiliar with Selection Sort, we recommend watching the Conceptual
 #   Overview section of this question's video explanation before starting to code.
-# </p>
-# <h3>Sample Input</h3>
-# <pre><span class="CodeEditor-promptParameter">array</span> = [8, 5, 2, 9, 5, 6, 3]
-# </pre>
-# <h3>Sample Output</h3>
-# <pre>[2, 3, 5, 5, 6, 8, 9]
-# </pre></div>
+# 
+# Sample Input
+#  = [8, 5, 2, 9, 5, 6, 3]
+# 
+# Sample Output=
+# [2, 3, 5, 5, 6, 8, 9]
 
 def selectionSort(array):
   currentIdx = 0
